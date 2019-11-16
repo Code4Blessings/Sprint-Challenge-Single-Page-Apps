f86f68a3255c682813faa6088b5b21fb55294de0
Code4Blessings <robinwarden@code4blessings.com>
2019-11-16 13:39:29 -0500

    Fixing searchform

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, Link } from "react-router-dom";
 import axios from "axios";
 import Header from "./components/Header.js";
@@ -17,37 +17,11 @@ const Nav = styled.nav`
 `;
 
 function App() {
-   const [searchRM, setSearchRM] = useState("");
-   const [searchResults, setSearchResults] = useState([]);
-
-   const handleChange = event => {
-     setSearchRM(event.target.value)
-   }
-    useEffect(() => {
-      
-        axios
-          .get('https://rickandmortyapi.com/api/character/')
-          .then((response) => {
-            const cartoonResults = response.data.results.filter(character => character.toLowercase().includes(searchRM.toLowerCase()))
-            setSearchResults(cartoonResults)
-          })
-          .catch((error) => {
-            console.log('Data returned an error', error)
-          })
-      },[searchRM]);
-
+   
   return (
     <div className="App">
       <Nav>
        <Header />
-       <SearchForm onChange={handleChange} value={searchRM} />
-       <div className="rick-morty-list">
-        <ul>
-         {searchResults.map(character => (
-           <li>{character}</li>
-         ))}
-        </ul>
-       </div>
         <div className='nav-links'>
           <Link to="/">Home</Link>
           <Link to="/character-list">FanPage</Link>
